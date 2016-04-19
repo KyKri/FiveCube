1471cace962ff67b6c61799ef993ef45a7e26fc8
KyKri <krkrick@gmail.com>
2016-04-19 11:01:57 -0700

    Outlined possible methods that this program will need

--- FiveCube.java
+++ FiveCube.java
@@ -6,5 +6,40 @@ Solves a 5x5x5 cube
 */
 
 public class FiveCube{
+
+	//starts the program
+	public FiveCube(){
+		makeCube();
+		makePieces();
+	}
+
+	//makes the cube 5x5x5 that will be filled
+	public makeCube(){
+
+	}
+
+	//makes the 25 pieces to play with
+	public makePieces(){
+
+	}
+
+	//this is the algorithm that will assemble the pieces
+	public fillCube(){
+
+	}
+
+	//finds all possible moves
+	public makeMove(){
+
+	}
+
+	//helps makeMove find all orientations of each piece
+	public findOrientations(){
+
+	}
+
+	public Static void main(String[] args){
+		FiveCube();
+	}
 	
 }
