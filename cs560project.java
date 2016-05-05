2c7eb840a6c4abeaecc02cb919e877483fbfb3df
KyKri <krkrick@gmail.com>
2016-05-04 17:34:49 -0700

    Altered Roots code to read from the input file

--- cs560project.java
+++ cs560project.java
@@ -1,264 +1,183 @@
-public class cs560project {
+public class cs560project
+{
+    public static int N;  // hint assumes 3x3x3 solution ciube
+    public static int P;  // hint assumes 7 pieces in puzzle
 
-	public static final int N = 3;
-	public static int[][][] mapTable;
-	public static int[] xInvTable;
-	public static int[] yInvTable;
-	public static int[] zInvTable;
+    public static int[][][] mapTable ;
+    public static int[] xInvTable ;
+    public static int[] yInvTable ;
+    public static int[] zInvTable ;
+    
+    public static piece3D[] pieces ;
+    
+    public static int[] solution ;
+    public static boolean continuing ;
+    
+    public static char[][][] solutionCharMatrix ;
 
-	public cs560project(){
-		mapTable = new int[N][N][N];
-		xInvTable = new int[N*N*N];
-		yInvTable = new int[N*N*N];
-		zInvTable = new int[N*N*N];
-		int count = 0;
-		for (int ix=0; ix<N; ix++)	
-			for (int iy=0; iy<N; iy++)
-				for (int iz=0; iz<N; iz++)	{
-					mapTable[ix][iy][iz] = 1 << count;
-					xInvTable[count] = ix;
-					yInvTable[count] = iy;
-					zInvTable[count] = iz;
-					count++;
-					}
-		
-		piece3D p = new piece3D('A',4);
-		p.setCube(0,0,0,0);
-		p.setCube(1,1,0,0);
-		p.setCube(2,1,0,1);
-		p.setCube(3,2,0,1);
-		p.findAllPossiblePositionsAsBitmaps();
-	}
-	
-	public static void main(String[] args){
-		new cs560project();
-	}
+    public static int[] inputs;
+    
+    
+    //=====================================================================================================
+    
+    
+    public static void outputSolution( ) 
+    {
+      //--- fill 3x3x3 char matrix with piece ID's per solution piece placements decoded from solution bitmaps
+      
+        for ( int whichPiece = 0 ; whichPiece < 7 ; whichPiece++ )
+          {
+            char idChar = pieces[whichPiece].pieceID ;
+            int integratedBitmap = solution[whichPiece] ;
+            for ( int i = 0 ; i < N*N*N ; i++ ) 
+              {
+                if ( integratedBitmap % 2 == 1 ) 
+                  solutionCharMatrix[xInvTable[i]][yInvTable[i]][zInvTable[i]] = idChar ; 
+                integratedBitmap >>= 1 ;                          
+              }
+          }
 
-	private class bitmapNode {
-	int bitmap;
-	bitmapNode link;
-	
-	public bitmapNode()	{
-		bitmap = 0;
-		link = null;
-	}
+      //--- output 3x3x3 char matrix showing pieceID's at each location, layer-by-layer in z- direction    
+          
+        System.out.println( "HERE IS THE SOLUTION TO THE PROBLEM INSTANCE DISTRIBUTED IN CLASS" ) ;
+        System.out.println( "AS SOLVED BY THE PROGRAM WHOSE SOURCE CODE IS GIVEN AS HINT #2" ) ;
+        System.out.println( ) ;
+        System.out.println( "EACH CELL OF THE 3x3x3 SOLUTION CUBE IS LABELED WITH THE CHAR PIECE-ID OF THE PIECE" ) ;
+        System.out.println( "TO WHICH THE UNIT CUBE OCCUPYING THAT CELL BELONGS (OUTPUT LAYER-BY-LAYER IN Z-ORDER)" ) ;
+        System.out.println( ) ;
+        
+        for ( int z = 2 ; z >= 0 ; z-- ) 
+          {
+            System.out.println( ) ;
+            System.out.println( "z = " + z + " layer" ) ;
+            System.out.println( ) ;
+            for ( int x = 2 ; x >= 0 ; x-- ) 
+              {
+                for ( int y = 2 ; y >= 0 ; y-- ) 
+                  System.out.print( solutionCharMatrix[x][y][z] ) ;
+                System.out.println( ) ;
+              }            
+          }      
 
-	public bitmapNode(int map)	{
-		bitmap = map;
-		link = null;
-		}
-	}
+    }
 
-	private class piece3D {
+    
+    //=====================================================================================================
+    
+    
+    public static void DFS ( int level , int priorPartialSolution ) 
+    {
+      //--- recursive depth-first search to search for solution among all possible piece placement combinations 
+        
+        if ( level == P ) 
+            {
+               outputSolution( ) ;
+               continuing = false ;      
+            }
+          else
+            {
+              bitmapNode ptr = pieces[level].possiblePositionsBitmaps ;
+              while ( (ptr != null) && continuing )
+                {
+                  int overlap = priorPartialSolution & ptr.bitmap ;
+                  if ( overlap == 0 ) // no overlap
+                    {
+                      solution[level] = ptr.bitmap ;
+                      DFS ( level+1 , priorPartialSolution | ptr.bitmap ) ;
+                    }
+                  ptr = ptr.link ;
+                }
+            }
+    }
+    
+    
+    //=====================================================================================================
+    public cs560project(int[] inputs){
+      //--- instantiate and compute table for forward and co-inverse bitmap mapping(s)
+        N = inputs[0];
+        P = inputs[1];
+        
+        mapTable  = new int[N][N][N] ;
+        xInvTable = new int[N*N*N] ;
+        yInvTable = new int[N*N*N] ;
+        zInvTable = new int[N*N*N] ;
+        int count = 0 ;
+        for ( int ix = 0 ; ix < N ; ix++ ) 
+          for ( int iy = 0 ; iy < N ; iy++ ) 
+            for ( int iz = 0 ; iz < N ; iz++ )
+              {
+                mapTable[ix][iy][iz] = 1 << count ;
+                xInvTable[count] = ix ;
+                yInvTable[count] = iy ;
+                zInvTable[count] = iz ;
+                count++ ;
+              }
 
-	point3D[] cubes;
-	int nCubes;
-	char pieceID;
-	bitmapNode possiblePositionsBitmaps;
-	int xMin, xMax, xPan;
-	int yMin, yMax, yPan;
-	int zMin, zMax, zPan;
-	
-	public piece3D(char ID, int size)	{
-		nCubes = size;
-		pieceID = ID;
-		cubes = new point3D[size];
-		for (int i=0; i<size; i++)
-			cubes[i] = new point3D();
-		possiblePositionsBitmaps = null;
-		}
-	
-	public void setCube(int whichCube, int ix, int iy, int iz)	{
-		cubes[whichCube].x = ix;
-		cubes[whichCube].y = iy;
-		cubes[whichCube].z = iz;
-		}
-	
-	public piece3D applyIsometry(int isometryNumber)	{
-		piece3D p = new piece3D(pieceID, nCubes);
-		
-		for (int whichCube=0; whichCube<p.nCubes; whichCube++)	{
-			int x = cubes[whichCube].x;
-			int y = cubes[whichCube].y;
-			int z = cubes[whichCube].z;
-			switch (isometryNumber)	{
-				case 1: p.cubes[whichCube].x = x;
-						p.cubes[whichCube].y = y;
-						p.cubes[whichCube].z = z;
-						break;
-				case 2: p.cubes[whichCube].x = y;
-						p.cubes[whichCube].y = x;
-						p.cubes[whichCube].z = z;
-						break;
-				case 3: p.cubes[whichCube].x = z;
-						p.cubes[whichCube].y = x;
-						p.cubes[whichCube].z = y;
-						break;
-				case 4: p.cubes[whichCube].x = -y;
-						p.cubes[whichCube].y = x;
-						p.cubes[whichCube].z = z;
-						break;
-				case 5: p.cubes[whichCube].x = -z;
-						p.cubes[whichCube].y = y;
-						p.cubes[whichCube].z = x;
-						break;
-				case 6: p.cubes[whichCube].x = -x;
-						p.cubes[whichCube].y = z;
-						p.cubes[whichCube].z = y;
-						break;
-				case 7: p.cubes[whichCube].x = -x;
-						p.cubes[whichCube].y = -y;
-						p.cubes[whichCube].z = z;
-						break;
-				case 8: p.cubes[whichCube].x = -y;
-						p.cubes[whichCube].y = -z;
-						p.cubes[whichCube].z = x;
-						break;
-				case 9: p.cubes[whichCube].x = -z;
-						p.cubes[whichCube].y = -x;
-						p.cubes[whichCube].z = y;
-						break;
-				case 10:p.cubes[whichCube].x = y;
-						p.cubes[whichCube].y = -x;
-						p.cubes[whichCube].z = z;
-						break;
-				case 11:p.cubes[whichCube].x = z;
-						p.cubes[whichCube].y = -y;
-						p.cubes[whichCube].z = x;
-						break;
-				case 12:p.cubes[whichCube].x = x;
-						p.cubes[whichCube].y = -z;
-						p.cubes[whichCube].z = y;
-						break;
-				case 13:p.cubes[whichCube].x = x;
-						p.cubes[whichCube].y = z;
-						p.cubes[whichCube].z = -y;
-						break;
-				case 14:p.cubes[whichCube].x = y;
-						p.cubes[whichCube].y = x;
-						p.cubes[whichCube].z = -z;
-						break;
-				case 15:p.cubes[whichCube].x = z;
-						p.cubes[whichCube].y = y;
-						p.cubes[whichCube].z = -x;
-						break;
-				case 16:p.cubes[whichCube].x = -z;
-						p.cubes[whichCube].y = x;
-						p.cubes[whichCube].z = -y;
-						break;
-				case 17:p.cubes[whichCube].x = -x;
-						p.cubes[whichCube].y = y;
-						p.cubes[whichCube].z = -z;
-						break;
-				case 18:p.cubes[whichCube].x = -y;
-						p.cubes[whichCube].y = z;
-						p.cubes[whichCube].z = -x;
-						break;
-				case 19:p.cubes[whichCube].x = -x;
-						p.cubes[whichCube].y = -z;
-						p.cubes[whichCube].z = -y;
-						break;
-				case 20:p.cubes[whichCube].x = -z;
-						p.cubes[whichCube].y = -x;
-						p.cubes[whichCube].z = -y;
-						break;
-				case 21:p.cubes[whichCube].x = -y;
-						p.cubes[whichCube].y = -x;
-						p.cubes[whichCube].z = -z;
-						break;
-				case 22:p.cubes[whichCube].x = z;
-						p.cubes[whichCube].y = -x;
-						p.cubes[whichCube].z = -y;
-						break;
-				case 23:p.cubes[whichCube].x = y;
-						p.cubes[whichCube].y = -z;
-						p.cubes[whichCube].z = -x;
-						break;
-				case 24:p.cubes[whichCube].x = x;
-						p.cubes[whichCube].y = -y;
-						p.cubes[whichCube].z = -z;
-						break;
-				}
-			}
-		return p;
-		}
-	
-	public void normalizePiece()	{
-		xMin = 999; xMax = -999;
-		yMin = 999; yMax = -999;
-		zMin = 999; zMax = -999;
-		
-		for (int whichCube=0; whichCube<nCubes; whichCube++)	{
-			if (cubes[whichCube].x < xMin) xMin = cubes[whichCube].x;
-			if (cubes[whichCube].y < yMin) yMin = cubes[whichCube].y;
-			if (cubes[whichCube].z < zMin) zMin = cubes[whichCube].z;
-			if (cubes[whichCube].x > xMax) xMax = cubes[whichCube].x;
-			if (cubes[whichCube].y > yMax) yMax = cubes[whichCube].y;
-			if (cubes[whichCube].z > zMax) zMax = cubes[whichCube].z;
-			}
-		
-		for (int whichCube=0; whichCube<nCubes; whichCube++)	{
-			cubes[whichCube].x -= xMin;
-			cubes[whichCube].y -= yMin;
-			cubes[whichCube].z -= zMin;
-			}
-		
-		xPan = cs560project.N - (xMax - xMin +1);
-		yPan = cs560project.N - (yMax - yMin +1);
-		zPan = cs560project.N - (zMax - zMin +1);
-		}
-	
-	public void findAllPossiblePositionsAsBitmaps()	{
-		int positionIndex = 0;
-		for (int whichIsometry=0; whichIsometry<=24; whichIsometry++)	{
-			piece3D q = applyIsometry(whichIsometry);
-			q.normalizePiece();
-			for (int dx=0; dx <= q.xPan; dx++)
-				for (int dy=0; dy <= q.yPan; dy++)
-					for (int dz=0; dz <= q.zPan; dz++)	{
-						bitmapNode b = new bitmapNode();
-						for (int whichCube=0; whichCube<q.nCubes; whichCube++)	{
-							int x = q.cubes[whichCube].x + dx;
-							int y = q.cubes[whichCube].y + dy;
-							int z = q.cubes[whichCube].z + dz;
-							b.bitmap |= cs560project.mapTable[x][y][z];
-							}
-						if (possiblePositionsBitmaps == null)
-							possiblePositionsBitmaps = b;
-						else	{
-							bitmapNode p;
-							p = possiblePositionsBitmaps;
-							while ((p.link != null) && (p.bitmap != b.bitmap))
-								p = p.link;
-							if (p.bitmap != b.bitmap)	{
-								positionIndex++;
-								System.out.println("======= " + positionIndex);
-								
-								p.link = b;
-								System.out.println("integrated bitmap " + b.bitmap);
-								}
-							}
-						}
-			}
-		}
+      //--- instantiate solution bitmap vector and solution character matrix  
+      
+        solution = new int[7] ;
+        solutionCharMatrix = new char[N][N][N] ;  
+            
+      //--- instantiate the pieces comprising the puzzle, as an array of piece3D objects indexed from 0     
+            
+        pieces = new piece3D[7] ; // for this hint, the puzzle is assumed to use 7 pieces      
+      
+        pieces[0] = new piece3D( 'A' , 4 ) ;
+        pieces[0].setCube(0,0,0,0) ;  // unit cubes comprising a piece are indexed from 0
+        pieces[0].setCube(1,1,0,0) ;
+        pieces[0].setCube(2,2,0,0) ;
+        pieces[0].setCube(3,0,0,1) ;
+      
+        pieces[1] = new piece3D( 'B' , 4 ) ;
+        pieces[1].setCube(0,0,0,0) ;
+        pieces[1].setCube(1,1,0,0) ;
+        pieces[1].setCube(2,1,0,1) ;
+        pieces[1].setCube(3,2,0,1) ;
+      
+        pieces[2] = new piece3D( 'C' , 3 ) ;
+        pieces[2].setCube(0,0,0,0) ;
+        pieces[2].setCube(1,1,0,0) ;
+        pieces[2].setCube(2,1,0,1) ;
+      
+        pieces[3] = new piece3D( 'D' , 5 ) ;
+        pieces[3].setCube(0,0,0,0) ;
+        pieces[3].setCube(1,1,0,0) ;
+        pieces[3].setCube(2,0,1,0) ;
+        pieces[3].setCube(3,1,1,0) ;
+        pieces[3].setCube(4,0,0,1) ;
+      
+        pieces[4] = new piece3D( 'E' , 5 ) ;
+        pieces[4].setCube(0,0,0,0) ;
+        pieces[4].setCube(1,1,0,0) ;
+        pieces[4].setCube(2,1,0,1) ;
+        pieces[4].setCube(3,0,1,0) ;
+        pieces[4].setCube(4,0,1,1) ;
+      
+        pieces[5] = new piece3D( 'F' , 3 ) ;
+        pieces[5].setCube(0,0,0,0) ;
+        pieces[5].setCube(1,1,0,0) ;
+        pieces[5].setCube(2,2,0,0) ;
+      
+        pieces[6] = new piece3D( 'G' , 3 ) ;
+        pieces[6].setCube(0,0,0,0) ;
+        pieces[6].setCube(1,0,1,0) ;
+        pieces[6].setCube(2,0,2,0) ;
 
-	}
-
-
-	public class point3D {
-	
-	int x;
-	int y;
-	int z;
-	
-	public point3D()	{
-		}
-	
-	public point3D(int ix, int iy, int iz)	{
-		x=ix;
-		y=iy;
-		z=iz;
-		}
-
-	}	
+      //--- compute lists of possible positions for each piece
+      
+        for ( int whichPiece = 0 ; whichPiece < 7 ; whichPiece++ ) 
+          pieces[whichPiece].findAllPossiblePositionsAsBitmaps( ) ;
+        
+      //--- DFS to examine all combinations of piece placement for each of seven pieeces to find solution
+      
+        continuing = true ;
+        DFS ( 0 , 0 ) ;
+    }
+    
+    public static void main(String[] args)
+    {
+      FileParser parse = new FileParser(args[0]);
+      inputs = parse.results;
+      new cs560project(inputs);
+    }
 }
