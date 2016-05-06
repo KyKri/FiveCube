69f8203504f85e93f298f2ddc0f00651effb8e69
KyKri <krkrick@gmail.com>
2016-05-05 18:41:10 -0700

    Much faster. Ditched Scanner for BufferedReader

--- FileParser.java
+++ FileParser.java
@@ -4,42 +4,42 @@ import java.util.*;
 public class FileParser{
 	int[] results;
 	char[] charArr;
-	ArrayList<String> strArrList;
 	String[] strArr;
 
 	public FileParser(String input){
-		readInputFile(input);
+		bfRead(input);
 	}
 
-	//takes a file name as a string, returns file as an array of ints
-	public int[] readInputFile(String fileName){
-		File file = new File(fileName);
-		String contents = "";
+	public int[] bfRead(String fileName){
 		try{
-			Scanner scan = new Scanner(file);
-			while(scan.hasNextLine()){
-				contents = contents + scan.nextLine();
+			BufferedReader bf = new BufferedReader(new FileReader(fileName));
+			int[] temp = new int[1000];
+			int size = 0;
+			String line = "";
+			int i = 0;
+			try{
+				while ((line=bf.readLine())!=null){
+					char[] chars = line.toCharArray();
+					for(int j=0; j<line.length();j++){
+						temp[i++] = Character.getNumericValue(chars[j]);
+						size++;
+					}
+				}
+				results = new int[size];
+				for(int x=0; x<size; x++)
+					results[x] = temp[x];
+				return results;
+			}catch(IOException E){
+				System.out.println("ran into IOException");
+				return null;
 			}
-
-			System.out.println(contents);
-			charArr = new char[contents.length()];
-			charArr = contents.toCharArray();
-
-			results = new int[charArr.length];
-			for(int i=0; i<results.length; i++){
-				results[i] = Character.getNumericValue(charArr[i]);
-			}
-
-			return results;
-		}catch (FileNotFoundException E){
-			System.out.println("File Not Found");
+		}catch(FileNotFoundException E){
+			System.out.println("File not found");
 			return null;
 		}
-
 	}
-
-	/*public static void main(String[] args){
-		String inputFile = args[0];
-		new FileParser(inputFile);
+	/*
+	public static void main(String[] args){
+		new FileParser(args[0]);
 	}*/
 }
